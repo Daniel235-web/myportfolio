37946a956cdb5dbf0c8e7a138a69c0e4ffca55e9
Daniel235-web <samueledaniel235@gmail.com>
2023-11-02 06:45:15 +0000

    changes

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import centdoor from '../../assets/centdoor.png';
 import soquify from '../../assets/soquify.png';
@@ -6,8 +6,22 @@ import Linkedin from '../../assets/linkedin.png';
 import TwitterIcon from '../../assets/twitter.png';
 import GithubIcon from '../../assets/github.png';
 import CvIcon from '../../assets/CV1.png';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ho7y46d', 'template_1tcczsn', form.current, 'N8LyrRiRdZAMzt-Nq')
+      .then((result) => {
+          console.log(result.text);
+          alert('Email sent !');
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <section id="contactPage">
         <div id="clients">
@@ -25,23 +39,31 @@ const Contact = () => {
         <div id="contact">
           <h1 className='contactPageTitle'>Contact Me</h1>
           <span className='contactDesc'>Let's connect to discuss how I can bring value to your organization.</span>
-          <form className='contactForm'>
-            <input type='text' className='name' placeholder='Your Name'/>
-            <input  type='email' className='email' placeholder='Your Email'/> 
+          <form className='contactForm' ref={form} onSubmit={sendEmail}>
+             <input type='text' className='name' placeholder='Your Name' name='from_name' required />
+             <input type='email' className='email' placeholder='Your Email' name='your_email' required />
             <textarea className='msg' name='message' rows='5' placeholder='Type Your Message'></textarea>
             <button type='submit' value='Send' className='submitBtn'>Submit</button>
             <div className='links'>
-              <img src={Linkedin} alt='FacebookIcon' className='link'/>
-              <img src={TwitterIcon} alt='TwitterIcon' className='link'/>
-              <img src={GithubIcon } alt='YoutubeIcon' className='link'/>
-              <img src={CvIcon } alt='InstagramIcon' className='link'/>
-              
+              <a href="https://www.linkedin.com/in/emmanuel-samuel-813507220/" target="_blank" rel="noopener noreferrer">
+                <img src={Linkedin} alt='LinkedIn' className='link'/>
+              </a>
+              <a href="https://twitter.com/Samuel475095031" target="_blank" rel="noopener noreferrer">
+                <img src={TwitterIcon} alt='Twitter' className='link'/>
+              </a>
+              <a href="https://github.com/Daniel235-web" target="_blank" rel="noopener noreferrer">
+                 <img src={GithubIcon} alt='GitHub' className='link'/>
+              </a>
+              <a href="https://www.example.com/your-cv-url.pdf" target="_blank" rel="noopener noreferrer">
+                 <img src={CvIcon} alt='CV' className='link'/>
+              </a>
             </div>
+
           </form>
 
         </div>
         <div className='footer'>
-        copyright &# 052: 2023 Samuel Emmanuel. All right received.
+        &copy; 2023 Samuel Emmanuel. All rights reserved.
     </div>
     </section>
   );
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contact.png';
 import { Link } from 'react-router-dom';
+import menu from '../../assets/menu.png';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className="logo" width={50} height={50} />
@@ -19,6 +21,15 @@ const Navbar = () => {
         <img src={contactImg} alt="" className="desktopMenuImg" />
         Get In Touch
       </Link>
+      <img src={menu} alt="Menu" className="mobMenu" onClick={()=>setShowMenu(!showMenu)} width={50} height={50} />
+      <div className="navMenu" style={{display: showMenu? 'flex': 'none'}}>
+        <Link to="/" className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link to="/skills" className="listItem" onClick={()=>setShowMenu(false)}>About</Link>
+        <Link to="/caseStudies" className="listItem" onClick={()=>setShowMenu(false)}>Case-Studies</Link>
+        <Link to="/clients" className="listItem" onClick={()=>setShowMenu(false)}>My Clients</Link>
+        <Link to="/clients" className="listItem" onClick={()=>setShowMenu(false)}>Get In Touch</Link>
+      </div>
+      
     </nav>
   );
 }
