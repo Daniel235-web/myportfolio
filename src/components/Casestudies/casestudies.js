8d57d07fd023030531944ef0815a009cfabfbed5
Daniel235-web <samueledaniel235@gmail.com>
2023-11-03 13:51:12 +0000

    casestudies

--- src/components/Casestudies/casestudies.js
+++ src/components/Casestudies/casestudies.js
@@ -4,6 +4,7 @@ import Estate1 from '../../assets/estate1.jpg';
 import Soquify from '../../assets/soquiify (1).png';
 import Telecom from '../../assets/telecom.png';
 
+
 const Casestudies = () => {
   return (
     <section id="casestudies">
@@ -16,13 +17,52 @@ const Casestudies = () => {
       </span>
       <div className="caseImgs">
         <a href="https://mern-estate-ya0i.onrender.com">
-          <img src={Estate1} alt="profile" className="caseImg" />
+          <div className="caseImg">
+            <img src={Estate1} alt="profile" />
+            <div className="caseDescOverlay">
+              <p>
+                <h2>Modern Real Estate Website</h2>
+                MongoDB, React, Expressjs and Nodejs.
+                <br /> Advanced Authentication: Implemented JWT,
+                <br /> Firebase, and Google OAuth for secure and seamless user
+                access, Implement cutting-edge search features to help users
+                find what they're looking for and more..
+              </p>
+            </div>
+          </div>
         </a>
         <a href="https://souqify.com/">
-          <img src={Soquify} alt="profile" className="caseImg" />
+          <div className="caseImg">
+            <img src={Soquify} alt="profile" />
+            <div className="caseDescOverlay">
+              <p>
+                <h2>Souqify </h2>
+                Your plug for house finishing products.
+                <br />
+                As a web developer and a web scraper designed to extract
+                valuable data and insights about Souqify, a dynamic and
+                innovative company specializing in house finishing products.
+                <h1> Liquid, Javascript, CSS/SCSS</h1>
+              </p>
+            </div>
+          </div>
         </a>
         <a href="https://centdoor.com/index.php/services/">
-          <img src={Telecom} alt="profile" className="caseImg" />
+          <div className="caseImg">
+            <img src={Telecom} alt="profile" />
+            <div className="caseDescOverlay">
+              <p>
+                <h2>Noc</h2>
+                Troubleshooting.
+                Security Monitoring.
+                Monitoring Network Infrastructure.
+                Alert Handling.
+
+
+
+              </p>
+            </div>
+          </div>
         </a>
       </div>
     </section>
